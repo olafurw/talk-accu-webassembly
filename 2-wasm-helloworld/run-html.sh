81ebf89e2577cf9e1bcc9092712039f529212f46
Olafur Waage <olafurw@gmail.com>
2022-11-18 17:15:31 +0100

    changing run port.

--- 2-wasm-helloworld/run-html.sh
+++ 2-wasm-helloworld/run-html.sh
@@ -1,1 +1,1 @@
-emrun --port 8080 --browser "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" hello_world.html;
+emrun --port 8001 --browser "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" hello_world.html;
